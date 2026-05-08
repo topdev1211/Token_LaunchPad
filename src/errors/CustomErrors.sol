6ef0f739a79c131f0b892d62597a36b3e1a8fd17
topdev1211 <wafiuddin1211@gmail.com>
2026-05-08 21:39:12 +0900

    chore: added errors file

---
+++ src/errors/CustomErrors.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+// BondingCurveFactory
+string constant INVALID_BC_FACTORY_OWNER = "BondingCurveFactory: INVALID_BC_FACTORY_OWNER";
+string constant INVALID_GNAD = "BondingCurveFactory: INVALID_GNAD";
+
+// BondingCurveLibrary
+string constant INVALID_INPUT_BC_LIBRARY = "BondingCurveLibrary: INVALID_INPUT_BC_LIBRARY";
+string constant INSUFFICIENT_BC_LIQUIDITY = "BondingCurveLibrary: INSUFFICIENT_BC_LIQUIDITY";
+string constant INVALID_AMOUNT_OUT = "BondingCurveLibrary: INVALID_AMOUNT_OUT";
+
+// BondingCurve
+string constant INVALID_FACTORY_ADDRESS = "BondingCurve: INVALID_FACTORY_ADDRESS";
+string constant INVALID_IT_IS_LOCKED = "BondingCurve: INVALID_IT_IS_LOCKED";
+string constant INVALID_IT_IS_UNLOCKED = "BondingCurve: INVALID_IT_IS_UNLOCKED";
+string constant INVALID_LOCKED_AMOUNT = "BondingCurve: INVALID_LOCKED_AMOUNT";
+string constant INVALID_RECIPIENT = "BondingCurve: INVALID_RECIPIENT";
+string constant INVALID_K = "BondingCurve: INVALID_K";
+string constant INVALID_ALREADY_LISTED = "BondingCurve: INVALID_ALREADY_LISTED";
+string constant INVALID_GNAD_ADDRESS = "BondingCurve: INVALID_GNAD_ADDRESS";
+
+// GNad
+string constant TIME_EXPIRED = "GNad: TIME_EXPIRED";
+string constant ALREADY_INITIALIZED = "GNad: ALREADY_INITIALIZED";
+string constant INVALID_FEE = "GNad: INVALID_FEE";
+string constant INVALID_MON_AMOUNT = "GNad: INVALID_MON_AMOUNT";
+string constant INVALID_AMOUNT_IN = "GNad: INVALID_AMOUNT_IN";
+string constant INVALID_ALLOWANCE = "GNad: INVALID_ALLOWANCE";
+string constant INVALID_AMOUNT_IN_MAX = "GNad: INVALID_AMOUNT_IN_MAX";
+string constant TRANSFER_MON_FAILED = "GNad: TRANSFER_MON_FAILED";
